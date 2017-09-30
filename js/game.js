829351adc8eaf4bd485fd06e88ea3df90e5c0439
Agnieszka <agg.olech@gmail.com>
2017-09-30 19:44:18 +0200

    dodano opcję włączenia muzyki, wprowadzono zmiany wizualne gry

--- js/game.js
+++ js/game.js
@@ -52,35 +52,38 @@ addEventListener(
 var reset = function() {
   monster.x = canvas.width / 2;
   monster.y = canvas.height - monsterImage.height;
-  addNewHint();
-	
 };
 
 var addNewHint = function() {
   var hint = {};
   hint.x = Math.random() * (canvas.width - hintImage.width);
   hint.y = -hintImage.height;
-  hints.push(hint)
-  setTimeout(addNewHint, 500 + Math.random() * 4000);
+  hints.push(hint);
+  hintsTimeout = setTimeout(addNewHint, 500 + Math.random() * 4000);
+  console.log('hintAdded');
+};
+
+var stopAddingHints = function() {
+  clearTimeout(hintsTimeout);
 };
 
 var update = function(modifier) {
   if (37 in keysDown) {
-	var newXLeft = monster.x - monster.speed * modifier;
-	 if (newXLeft < 0) {
-		 monster.x = 0;
-	 } else { 
-		 monster.x = newXLeft;
-	 }
+    var newXLeft = monster.x - monster.speed * modifier;
+    if (newXLeft < 0) {
+      monster.x = 0;
+    } else {
+      monster.x = newXLeft;
+    }
   }
   if (39 in keysDown) {
-	  var newXRight = monster.x + monster.speed * modifier;
-		if (newXRight > (canvas.width - monsterImage.width)) {
-			monster.x = canvas.width - monsterImage.width;
-		} else {
-			monster.x = newXRight;
-			}
-  	}
+    var newXRight = monster.x + monster.speed * modifier;
+    if (newXRight > canvas.width - monsterImage.width) {
+      monster.x = canvas.width - monsterImage.width;
+    } else {
+      monster.x = newXRight;
+    }
+  }
 
   var hint = {};
   hint.x = Math.random() * (canvas.width - hintImage.width);
@@ -99,75 +102,81 @@ var render = function() {
   }
 
   if (hintReady) {
-	  for ( i = 0; i < hints.length; i++ ) {
-		var hint = hints[i];
-		ctx.drawImage(hintImage, hint.x, hint.y);
-	  }
+    for (i = 0; i < hints.length; i++) {
+      var hint = hints[i];
+      ctx.drawImage(hintImage, hint.x, hint.y);
+    }
   }
-	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	ctx.fillText("Score: " + hintsCaught, 20, 20);
+  ctx.fillStyle = "rgb(250, 250, 250)";
+  ctx.font = "26px 'Sigmar One'";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText("Score: " + hintsCaught, 20, 20);
 };
 
-  var gameStarted = false;
-  var intervalId = null;
-  function start() {
-    intervalId = setInterval(changeValue, 1000);
-	gameStarted = true;
-	document.getElementById("starter").disabled = true;
-	} 
+var gameStarted = false;
+var intervalId = null;
+function start() {
+  intervalId = setInterval(changeValue, 1000);
+  gameStarted = true;
+  document.getElementById("starter").disabled = true;
+  addNewHint();
+  reset();
+  hintsCaught = 0;
+}
 
-  var sec = 10;
-  document.getElementById("finalCountDown").innerHTML = "0 : " +sec;
-  var finish = function () {
-		setTimeout(alert, 0, 'Game over! Your score: ' + hintsCaught + ' tones catched :) Congrats!');
-	};
-  function changeValue() {
-	sec--
-	document.getElementById("finalCountDown").innerHTML = "0 : " +sec ;
-	  if (sec < 0) {
-		clearInterval (intervalId);
-		gameStarted = false;
-		finish();
-		setTimeout(document.getElementById("starter").disabled = false);
-		sec = 10;
-		document.getElementById("finalCountDown").innerHTML = "0 : " +sec;	
-	}
-	
+var sec = 30;
+document.getElementById("finalCountDown").innerHTML = "0 : " + sec;
+var finish = function() {
+  stopAddingHints();
+  setTimeout(
+    alert,
+    0,
+    "Game over! Your score: " + hintsCaught + " tones catched :) Congrats!"
+  );
 };
+function changeValue() {
+  sec--;
+  document.getElementById("finalCountDown").innerHTML = "0 : " + sec;
+  if (sec < 0) {
+    clearInterval(intervalId);
+    gameStarted = false;
+    finish();
+    setTimeout((document.getElementById("starter").disabled = false));
+    sec = 30;
+    document.getElementById("finalCountDown").innerHTML = "0 : " + sec;
+  }
+}
 
 var moveHintDown = function() {
-  for ( i = 0; i < hints.length; i++ ) {
-	var hint = hints[i];
-	hint.y = hint.y + 1;
-	}
+  for (i = 0; i < hints.length; i++) {
+    var hint = hints[i];
+    hint.y = hint.y + 1;
+  }
 };
 
 var detectCollision = function() {
-  for ( i = 0; i < hints.length; i++ ) {
-	var hint = hints[i]; 
-	if (
-		monster.x <= hint.x + hintImage.width
-		&& hint.x <= monster.x + monsterImage.width
-		&& monster.y <= hint.y + hintImage.height
-		&& hint.y <= monster.y + monsterImage.height
-	) {
-		++hintsCaught;
-		hints.splice(i, 1);
-	  }
-	}
+  for (i = 0; i < hints.length; i++) {
+    var hint = hints[i];
+    if (
+      monster.x <= hint.x + hintImage.width &&
+      hint.x <= monster.x + monsterImage.width &&
+      monster.y <= hint.y + hintImage.height &&
+      hint.y <= monster.y + monsterImage.height
+    ) {
+      ++hintsCaught;
+      hints.splice(i, 1);
+    }
+  }
 };
-
 
 var main = function() {
   var now = Date.now();
   var delta = now - then;
   if (gameStarted) {
-	  moveHintDown();
+    moveHintDown();
   }
-  
+
   update(delta / 1000);
   render();
 
@@ -178,7 +187,7 @@ var main = function() {
 
 var w = window;
 requestAnimationFrame =
-  w.requestAnimationFrame || 
+  w.requestAnimationFrame ||
   w.webkitRequestAnimationFrame ||
   w.msRequestAnimationFrame ||
   w.mozRequestAnimationFrame;
